4ceb5e902925b6ca83843724d0649f71ff11d191
Mateus Nunes <mateusnunesnunes487@gmail.com>
2020-04-27 19:28:11 -0300

    Commit before merge

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/login',function(req,res){
   let resultado = req.body.senha + req.body.name
   var passwordHashed = md5(resultado);
   try {
-    con.query("SELECT * FROM Users WHERE nome = '"+user_name+"' AND senha = '"+passwordHashed+"'",function (error,rows,fields) {
+    con.query("SELECT * FROM Users WHERE email = '"+req.body.email+"' AND senha = '"+passwordHashed+"'",function (error,rows,fields) {
         if (rows.length == 0){
             res.send('Login falho');
         }
@@ -83,11 +83,13 @@ app.post('/login',function(req,res){
 app.post('/register',function(req,res){
     let resultado = req.body.senha + req.body.name
     let senhaHashed = md5(resultado);
+    let idade = 2020 - parseInt(req.body.anoNascimento);
+
     try {
-        con.query("select * from Users where nome = '"+[req.body.name]+"'",function (error,rows,fields) {
+        con.query("select * from Users where email = '"+[req.body.email]+"'",function (error,rows,fields) {
             if (rows.length == 0){
                 try {
-                    con.query("INSERT INTO `Users` (`nome`,`email`,`idade`,`senha`,`peso`,`objetivo`) VALUES ('"+req.body.name+"','"+req.body.email+"','"+req.body.idade+"','"+senhaHashed+"','"+req.body.peso+"','"+req.body.objetivo+"')",function (error,rows,fields) {
+                    con.query("INSERT INTO `Users` (`nome`,`email`,`idade`,`senha`,`peso`,`objetivo`, `altura`) VALUES ('"+req.body.name+"','"+req.body.email+"','"+idade+"','"+senhaHashed+"','"+req.body.peso+"','"+req.body.objetivo+"','"+req.body.altura+"')",function (error,rows,fields) {
                         if(error){
                             console.log(error);
                             res.json({'error':error});
@@ -112,9 +114,69 @@ app.post('/register',function(req,res){
                 }
             }
             
+            
         });
     } catch (error) {
         res.json({'error':error});
     } 
 })
-
+app.post('/insertFood',function(req,res){
+    let nome = req.body.nome
+    let carboidratos = req.body.carboidratos
+    let gordura = req.body.gordura
+    let proteina = req.body.proteina
+    let descricao = req.body.descricao
+    let semana  = req.body.semana
+    let lastId = 0
+    try {
+        con.query("INSERT INTO `comidas`(`nome`, `carboidratos`, `proteinas`, `gorduras`, `descricao`) VALUES ('"+nome+"','"+carboidratos+"','"+proteina+"','"+gordura+"','"+descricao+"');SELECT `id` AS LastID FROM `comidas` WHERE `id` = @@Identity;",function (error,rows,fields) {
+            if(error){
+                res.json({'error':error});
+            } 
+            else{
+                rows[1].forEach(element => {
+                    lastId = element["LastID"]
+                });
+                con.query("INSERT INTO `relacaoComidasSemana`(`idComida`, `idSemana`) VALUES ('"+lastId+"','"+semana+"')",function (error,rows,fields) {
+                    if(error){
+                        res.json({'error':error});
+                    } 
+                    else{
+    
+                        res.json({"success":rows});
+                    } 
+                })
+            }
+            
+            
+        });
+    } catch (error) {
+        res.json({'error':error});
+    } 
+})
+app.get('/listaComidas',function(req,res){
+    let idsComidas = []
+    try {
+        con.query("SELECT `idComida` FROM `relacaoComidasSemana` WHERE `idSemana`='1'",function (error,rows,fields) {
+            if(error){
+                res.json({'error':error});
+            } 
+            else{
+                rows.forEach(element => {
+                    idsComidas.push(element["idComida"])
+                });
+                con.query("SELECT `nome`,`carboidratos`,`proteinas`,`gorduras`,`descricao` FROM `comidas` WHERE `id` IN ("+String(idsComidas).split(",")+")",function (error,rows,fields) {
+                    if(error){
+                        res.json({'error':error});
+                    } 
+                    else{
+                        res.json({'comidas':rows})   
+                    }
+                });
+            }
+            
+        });
+    } catch (error) {
+        res.json({'error':error});
+    } 
+})
